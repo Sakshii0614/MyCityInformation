f4bbaba1398f2db72a5279c2bb4f567cf421faf7
Sakshii0614 <pandey.sakshi0601@gmail.com>
2023-12-27 19:50:40 +0530

    Update

---
+++ Ragu-mysuru/app/src/main/java/com/example/raghunandan/ragumysuru/ui/SplashActivity.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 Kaushik N. Sanji
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.raghunandan.ragumysuru.ui;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.raghunandan.ragumysuru.R;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(splashscreen.this, Parheeksha.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
